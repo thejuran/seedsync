50a8ce3af858f5daf16421c4b55f1445fe76b532
Jules1651 <150754439+Jules1651@users.noreply.github.com>
2026-01-21 12:34:41 -0500

    Update setup_seedsync.sh

--- src/docker/test/e2e/configure/setup_seedsync.sh
+++ src/docker/test/e2e/configure/setup_seedsync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./wait-for-it.sh myapp:8800 -- echo "Seedsync app is up (before configuring)"
+./wait-for-it.sh myapp:8800 -t 60 -- echo "Seedsync app is up (before configuring)"
 curl -sS "http://myapp:8800/server/config/set/general/debug/true"; echo
 curl -sS "http://myapp:8800/server/config/set/general/verbose/true"; echo
 curl -sS "http://myapp:8800/server/config/set/lftp/local_path/%252Fdownloads"; echo
@@ -13,7 +13,8 @@ curl -sS "http://myapp:8800/server/config/set/autoqueue/patterns_only/true"; ech
 
 curl -sS "http://myapp:8800/server/command/restart"; echo
 
-./wait-for-it.sh myapp:8800 -- echo "Seedsync app is up (after configuring)"
+./wait-for-it.sh myapp:8800 -t 60 -- echo "Seedsync app is up (after configuring)"
 
 echo
 echo "Done configuring SeedSync app"
+
